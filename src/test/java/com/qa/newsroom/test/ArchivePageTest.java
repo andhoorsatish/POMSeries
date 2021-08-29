7499c68828c1b98646a451a5f70de578b8b8ca64
Satish Andhoor <andhoorsatish@gmail.com>
2021-08-29 13:18:37 -0500

    Satish branch

--- src/test/java/com/qa/newsroom/test/ArchivePageTest.java
+++ src/test/java/com/qa/newsroom/test/ArchivePageTest.java
@@ -1,13 +1,18 @@
 package com.qa.newsroom.test;
 
-import org.testng.annotations.Listeners;
+import org.openqa.selenium.WebDriver;
 
 import com.qa.newsroom.base.BaseTest;
-import com.qa.newsroom.testlisteners.ExtentReportListener;
 
 
 //@Listeners(ExtentReportListener.class)
 
 public class ArchivePageTest extends BaseTest{
+	
+	public ArchivePageTest(WebDriver driver) {
+		
+		System.out.println("Test out put");
+		
+	}
 
 }
